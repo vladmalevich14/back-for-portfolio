ba17d7a7f0d7835282863f51a68d8539f740ccbd
vladmalevich14 <vladmalevich1431@gmail.com>
2023-08-08 20:17:51 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3010
+app.use(cors())
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'vladmalevich1431@gmail.com',
+        pass: 'oaumvjsqlccongxf'
+    }
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.post('/sendMessage', async (req, res) => {
+
+    const {name, phone, email, message} = req.body
+    const info = await transporter.sendMail({
+        from: 'HR WANTS ME',
+        to: "Vlad",
+        subject: "HR WANTS ME",
+        html: `<b>Сообщение с вашего портфолио:</b>
+<div>
+name: ${name}
+</div>
+<div>
+phone: ${phone}
+</div>
+<div>
+email: ${email}
+</div>
+<div>
+message: ${message}
+</div>
+`
+    });
+    res.send('blablabla')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
