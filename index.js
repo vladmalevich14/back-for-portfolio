90d54ea5c86f5f40554f3b2b45419699d1834d85
vladmalevich14 <vladmalevich1431@gmail.com>
2023-08-09 13:55:57 +0300

    cors_change

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 app.use(cors())
-
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+    next();
+})
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
